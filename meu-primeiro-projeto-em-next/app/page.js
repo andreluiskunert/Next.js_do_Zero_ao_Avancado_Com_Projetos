07df4d9693ddaf9b83d47638bef27d79e34d6e58
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-02-21 23:19:03 -0300

    bora la codar com Next.js_preparação pra o proximo Trabalho Prático_2026

---
+++ meu-primeiro-projeto-em-next/app/page.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <marquee> Em breve minha aplicação com Next.js e Tailwind CSS!</marquee>
+        <h1 className={styles.title}>Meu Primeiro Projeto em Next.js</h1>
+        <p className={styles.description}>
+          
+          Bem-vindo ao meu primeiro projeto em Next.js! Este é um exemplo de
+          página inicial.
+          <marquee> Em breve minha aplicação com Next.js e Tailwind CSS!</marquee>
+        </p>
+      </main>
+    </div>
+  );
+}
