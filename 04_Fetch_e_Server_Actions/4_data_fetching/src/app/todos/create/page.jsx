8d35970de401b985f5c73fa434c506fc32f16fd2
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-03-27 22:53:03 -0300

    bora la codar com Next.js_2026

--- 04_Fetch_e_Server_Actions/4_data_fetching/src/app/todos/create/page.jsx
+++ 04_Fetch_e_Server_Actions/4_data_fetching/src/app/todos/create/page.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const TodoPage = () => {
   return (
     <div>
-      <h1>Criação de Todos</h1>
+      <h1 className="text-2X1 font-bold text-center mb-6">Criação de Todos</h1>
+      <form  className="flex-col gap-4 p-4 bg-white shadow-lag rounded-lg">
+        <label className="block text-sm font-medium text-gray-700">
+
+
+        </label>
+      </form>
     </div>
   )
 }
