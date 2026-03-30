484dbf47ea61bab1a82e6fdc5410a98d8a83e33f
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-03-29 23:43:16 -0300

    bora la codar com Next.js_2026

--- 04_Fetch_e_Server_Actions/4_data_fetching/src/app/todos/create/page.jsx
+++ 04_Fetch_e_Server_Actions/4_data_fetching/src/app/todos/create/page.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
 
 const TodoPage = () => {
+  const addTodo = async(FormData)=>{
+  "use server";
+  console.log(FormData)
+  }
   return (
-    <div>
+    <div className="max-w-md-auto mt-10">
       <h1 className="text-2X1 font-bold text-center mb-6">Criação de Todos</h1>
-      <form  className="flex-col gap-4 p-4 bg-white shadow-lag rounded-lg">
-        <label className="block text-sm font-medium text-gray-700">
-
+      <form action={addTodo} className="flex-col gap-4 p-4 bg-white shadow-lag rounded-lg">
+        <label htmlFor="titulo" className="block text-sm font-medium text-gray-700">
+           <h1> Criar novas Tarefas</h1>
+        <input
+        type="text"
+        id="titulo"
+        name="titulo"
+        placeholder="Inserir o Titulo:"
+        required
+         className="mt-1 px-4 border border-gray-300 rounded-md w-full" />
 
         </label>
+         <label htmlFor="descricao" className="block text-sm font-medium text-gray-700">
+           <h1> Descrição</h1>
+        <textarea
+        type="text"
+        id="descricao"
+        name="descricao"
+        placeholder="Inserir a descrição do produto:"
+        required
+         className="mt-1 px-4 py-2 border border-gray-300 rounded-md w-full h-32" />
+
+        </label>
+        <button 
+          type="submit"
+          className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">Criar Todo</button>
       </form>
     </div>
   )
