29e07ca8b921d8790690249d21585a4b4d94aaa1
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-04-04 16:38:30 -0300

    bora la codar com Next.js_2026

--- 04_Fetch_e_Server_Actions/4_data_fetching/src/app/page.js
+++ 04_Fetch_e_Server_Actions/4_data_fetching/src/app/page.js
@@ -10,7 +10,7 @@ export default async function Home() {
       <main className="container mx-auto p-4">
         <h1 className="text-2xl font-bold mb-4">Todos!</h1>
         <p> Bora rever materia</p>
-      
+      <todo></todo>
       </main>
     </div>
   );
