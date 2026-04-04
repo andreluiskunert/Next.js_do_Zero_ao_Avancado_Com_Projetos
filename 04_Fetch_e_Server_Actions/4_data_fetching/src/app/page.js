7b050098becffbd1dc1096f98c5e8cbcd86d68ba
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-04-03 22:35:00 -0300

    bora la codar com Next.js_2026_Revisão01_2026

---
+++ 04_Fetch_e_Server_Actions/4_data_fetching/src/app/page.js
@@ -0,0 +1,17 @@
+// import Button from "@/components/Button";
+import Link from "next/link";
+;
+
+export default async function Home() {
+  
+
+  return (
+    <div>
+      <main className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-4">Todos!</h1>
+        <p> Bora rever materia</p>
+      
+      </main>
+    </div>
+  );
+}
---
+++ 04_Fetch_e_Server_Actions/4_data_fetching/src/app/todos/create/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Todopage = () => {
+  return (
+    <div className="max-w-md mx-auto mt-10">
+        <h1  className="text-2x1 font-bold text-center m-6"> 
+       Criar nova tarefa
+        </h1>
+      
+    </div>
+  )
+}
+
+export default Todopage
