c005ab64ba3748acceab83da776e68fdd5858c74
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-04-05 23:35:31 -0300

    bora la codar com Next.js_2026

--- 04_Fetch_e_Server_Actions/4_data_fetching/src/app/todos/create/page.js
+++ 04_Fetch_e_Server_Actions/4_data_fetching/src/app/todos/create/page.js
@@ -1,18 +1,36 @@
 import React from 'react'
+import { db } from '@/db'
+import { redirect } from 'next/navigation'
 
 const Todopage = () => {
   const addTodo = async (formData) => {
     "use server";
-    console.log(formData)
-  }
+
+    const titulo = formData.get("titulo");
+    const descricao = formData.get("descricao");
+    const status = "pendente";
+
+    const todo = await db.todo.create({
+      data: {
+        titulo,
+        descricao,
+        status
+      }
+    });
+
+    console.log(todo);
+
+    redirect("/");
+  };
+
   return (
-     
     <div className="max-w-md mx-auto mt-10">
-        <h1  className="text-2x1 font-bold text-center m-6"> 
-       Criar nova tarefa
-        </h1>
-       <form action={addTodo} className="flex flex-col gap-4 p-4 bg-white shadow-lg rounded-lg">
-     <label
+      <h1 className="text-2xl font-bold text-center m-6">
+        Criar nova tarefa
+      </h1>
+
+      <form action={addTodo} className="flex flex-col gap-4 p-4 bg-white shadow-lg rounded-lg">
+        <label
           htmlFor="titulo"
           className="block text-sm font-medium text-gray-700"
         >
@@ -26,6 +44,7 @@ const Todopage = () => {
             className="mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 w-full"
           />
         </label>
+
         <label
           htmlFor="descricao"
           className="block text-sm font-medium text-gray-700"
@@ -39,6 +58,7 @@ const Todopage = () => {
             className="mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 w-full h-32 resize-none"
           ></textarea>
         </label>
+
         <button
           type="submit"
           className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
@@ -47,8 +67,7 @@ const Todopage = () => {
         </button>
       </form>
     </div>
-
   );
 };
 
-export default Todopage
+export default Todopage;
