bbef29f4ee790d9695826d70aa98267d756ca00b
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-04-13 01:33:33 -0300

    Bora La Codar com Next.js_2026

---
+++ 04_Fetch_e_Server_Actions/4_data_fetching/src/app/todos/[id]/page.js
@@ -0,0 +1,30 @@
+import { db } from '@/db'
+import { notFound } from 'next/navigation'
+import React from 'react'
+
+const TodoShow = async ({ params }) => {
+  const { id } = await params;
+  const todoId = Number(id);
+
+  if (Number.isNaN(todoId)) {
+    notFound();
+  }
+
+  const todo = await db.todo.findFirst({
+    where: {
+      id: todoId,
+    },
+  });
+
+  if (!todo) {
+    notFound();
+  }
+
+  return (
+    <div>
+      <h1>{todo.titulo}</h1>
+    </div>
+  );
+};
+
+export default TodoShow;
